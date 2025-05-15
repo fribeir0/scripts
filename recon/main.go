afe8e6229b23c1d7665eccecddcee85012b70a30
fribeir0 <felipecarv971@gmail.com>
2025-05-15 19:17:22 -0300

    v0.02

--- recon/main.go
+++ recon/main.go
@@ -10,21 +10,33 @@ func ExecCommands (command string, args ...string) ([]byte,error){
 	cmd:= exec.Command(command,args...)
 	stdout, err := cmd.Output()
 	if err != nil {
-		fmt.Println(err.Error())
+		return nil,err
 	}
-	return stdout, err 
+	return stdout, nil 
 
 }
 
 func main () {
 	fmt.Println("Iniciando reconhecimento...")
 	hname, err := ExecCommands("hostname")
-	fmt.Println("hostname:",hname,err.Error())
+	if err != nil {
+		fmt.Println("error",err)
+	}
+	fmt.Println("hostname",string(hname))
 	dir, err := ExecCommands("ls","-la","/")
-	fmt.Println("diretorios:",dir,err.Error())
+	if err != nil {
+		fmt.Println("erro ao listar:",err)
+	}
+		fmt.Println("diretorios em /",string(dir))
 	openP, err:= ExecCommands("ss","-tuln")
-	fmt.Println("open ports",openP,err.Error())
+	if err != nil {
+		fmt.Println("erro ao ver portas abertas",err)
+	}
+		fmt.Println("portas abertas:",openP)
 	psAux,err:= ExecCommands("psaux")
-	fmt.Println("process:",psAux,err.Error())
+	if err != nil {
+		fmt.Println("erro ao listar processos:",err)
+	}
+	fmt.Println("process:",psAux)
 
 }
